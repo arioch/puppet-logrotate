73dfc355e11857c891cc6bb70e81d9728e0f350c
Tom De Vylder <tom@penumbra.be>
2011-08-17 00:22:33 +0200

    passed puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,1 @@
-import "*"
+include '*'
--- manifests/logrotate.pp
+++ manifests/logrotate.pp
@@ -2,32 +2,32 @@ class logrotate {
 	define file (
 			$source, 
 			$log, 
-			$options = [ "weekly", "compress", "rotate 7", "missingok" ], 
-			$prerotate = "NONE", 
-			$postrotate = "NONE"
+			$options = [ 'weekly', 'compress', 'rotate 7', 'missingok' ], 
+			$prerotate = 'NONE', 
+			$postrotate = 'NONE'
 	) {
 	   	file { "/etc/logrotate.d/${name}":
 			ensure  => present,
 	    	owner   => root,
 	        group   => root,
 	        mode    => 644,
-	        content => template("logrotate/logrotate.erb"),
-	        require => Class["config"],
+	        content => template('logrotate/logrotate.erb'),
+	        require => Class['config'],
 		}
 	}
 	
 	class install {
-		package { "logrotate":
+		package { 'logrotate':
 			ensure => installed,
 		}
 	}
 	
 	class config {
-		file { "/etc/logrotate.d/":
+		file { '/etc/logrotate.d/':
 	    	owner  	=> root,
 	        group  	=> root,
 			ensure 	=> directory,
-			require	=> Class["install"],
+			require	=> Class['install'],
 	    }
 	}
 	
